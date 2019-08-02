1921e7c6738d62fe2d29510f85c22781a0c4b962
Zacharey Carey <zac4ncstate@aol.com>
2019-08-01 22:53:54 -0400

    bamazon

--- bamazon.sql
+++ bamazon.sql
@@ -1,12 +1,24 @@
-CREATE DATABASE bamazon;
-USE bamazon;
+CREATE DATABASE bamazon_db;
+USE bamazon_db;
 
 CREATE TABLE products
 (
-item_id
-product_name
-department_name
-price
-stock_quantity
+item_id INTEGER NOT NULL,
+product_name VARCHAR,
+department_name VARCHAR,
+price DECIMAL(10, 2) NOT NULL,
+stock_quantity INTEGER NOT NULL,
 PRIMARY KEY (item_id)
 );
+
+INSERT INTO products (product_name, department_name, price,stock_quantity)
+VALUES ('chicken', 'store', 2.99, 100);
+
+INSERT INTO products (product_name, department_name, price,stock_quantity)
+VALUES ('milk', 'store', 1.99, 90);
+
+INSERT INTO products (product_name, department_name, price,stock_quantity)
+VALUES ('steak', 'store', 7.99, 70);
+
+INSERT INTO products (product_name, department_name, price,stock_quantity)
+VALUES ('rice', 'store', 0.99, 1000);
--- customer.js
+++ customer.js
@@ -17,5 +17,68 @@ connection.connect(function(err) {
 });
 
 function displayProducts() {
-  var query = "SELECT * from products";
+  var query = "SELECT * from products", function(err, res) {
+    if (err) throw err;
+    console.log("Welcome to Bamazon");
+});
+
+function quantity() {
+
+  var table = new Table({
+      head: ['ID', 'Product', 'Department', 'Price', 'Stock'],
+      colWidths: [10, 30, 30, 30, 30]
+  });
+
+  listQuantity();
+
+  function listQuantity() {
+
+      connection.query("SELECT * FROM products", function(err, res) {
+          for (var i = 0; i < res.length; i++) {
+
+              var itemId = res[i].item_id,
+                  productName = res[i].product_name,
+                  departmentName = res[i].department_name,
+                  price = res[i].price,
+                  stockQuantity = res[i].stock_quantity;
+
+            table.push(
+                [itemId, productName, departmentName, price, stockQuantity]
+          );
+        }
+          console.log("");
+          console.log("Inventory");
+          console.log("");
+          console.log(table.toString());
+          continuePrompt();
+      });
+  }
 }
+
+function itemInfo() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What item would you like to buy?",
+      name: "product",
+      filter: Number
+    },
+
+    {
+      type: "input",
+      name: "quantity",
+      message: "How many would like to purchase?",
+      filter: Number
+    }
+
+function purchase(product, quantity) {
+  var listQuantity = product.stock_quantity - quantity;
+  connection.query(
+    [{stock_quantity: listQuantity}, {item_id: product.item_id}],
+    function(err, res) {
+      console.log();
+      displayProducts();
+    }
+  );
+}
+
